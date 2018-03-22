169ba95237fa12d18b10c1292d0ce863f8ec651d
Joeri Bakker <bakkerjoeri@gmail.com>
2018-03-22 14:36:19 +0100

    Add dynamic command method

---
+++ src/dynamic.js
@@ -0,0 +1,28 @@
+const getAllFilesFromPatterns             = require('./utilities/getAllFilesFromPatterns.js');
+const doesTemplateContainDynamicInclusion = require('./utilities/doesTemplateContainDynamicInclusion.js');
+
+/**
+ * Report templates with possible dynamic inclusions.
+ * 
+ * @param  {String[]} sourcePatterns
+ */
+function dynamic(sourcePatterns) {
+    let sources = getAllFilesFromPatterns(sourcePatterns);
+    
+    let sourcesUsingDynamicInclusion = sources.filter((source) => {
+        return doesTemplateContainDynamicInclusion(source);
+    });
+    
+    // Report the results
+    if (sourcesUsingDynamicInclusion.length === 0) {
+        console.log(`No templates using dynamic inclusion found.`);
+    } else {
+        console.log(`Dynamic inclusion found in the following ${sourcesUsingDynamicInclusion.length} templates:`);
+        
+        sourcesUsingDynamicInclusion.forEach((source) => {
+            console.log(`- ${path.basename(source.filepath)}`);
+        });
+    }
+}
+
+module.exports = dynamic;
